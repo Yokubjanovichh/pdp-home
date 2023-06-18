48a90a4416a427bbb6a9a97ee2bd1bc22db52175
yokubjanovich <yokubjanovich@gmail.com>
2023-06-18 10:35:12 +0500

    delete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
+import "./main.scss";
+
+
 export default class App extends React.Component {
   render() {
-    return <div className="container"></div>;
+    return (
+      <div className="container">
+
+      </div>
+    );
   }
 }
